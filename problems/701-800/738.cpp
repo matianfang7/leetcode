41bea0ff870a83b46d2ced79d6413f4d5eb5de50
matianfang <75879667+matianfang7@users.noreply.github.com>
2020-12-15 19:16:53 +0800

    Update 738.cpp

--- problems/701-800/738.cpp
+++ problems/701-800/738.cpp
@@ -23,3 +23,25 @@ public:
         return stoi(str);
     }
 };
+
+//leetcode solution
+class Solution {
+public:
+    int monotoneIncreasingDigits(int N) {
+        string strN = to_string(N);
+        int i = 1;
+        while (i < strN.length() && strN[i - 1] <= strN[i]) {
+            i += 1;
+        }
+        if (i < strN.length()) {
+            while (i > 0 && strN[i - 1] > strN[i]) {
+                strN[i - 1] -= 1;
+                i -= 1;
+            }
+            for (i += 1; i < strN.length(); ++i) {
+                strN[i] = '9';
+            }
+        }
+        return stoi(strN);
+    }
+};
